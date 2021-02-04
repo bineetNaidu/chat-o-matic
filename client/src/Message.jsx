7d23418474d4f540e01de70be3a21a1436447022
bineetNaidu <bineet.s.naidu@gmail.com>
2021-02-04 20:06:36 +0530

    ❇️ add Message Component

---
+++ client/src/Message.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_MESSAGES } from './queries';
+
+const Message = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES, {
+    pollInterval: 500,
+  });
+  if (!data) return null;
+
+  return (
+    <div>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          key={id}
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '0.5em',
+                border: '2px solid #e5e6ea',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Message;
