5b9d1f83579d5ccb1509ecdecb75d7c1e3712ad6
bineetNaidu <bineet.s.naidu@gmail.com>
2021-02-04 20:40:17 +0530

    🎉 add WebSocket Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chat from './Chat';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 import './index.css';
 
 const App = () => {
+  const link = new WebSocketLink({
+    uri: 'ws://localhost:4000',
+    options: {
+      reconnect: true,
+    },
+  });
+
   const client = new ApolloClient({
     uri: 'http://localhost:4000',
     cache: new InMemoryCache(),
+    link,
   });
+
   return (
     <ApolloProvider client={client}>
       <Chat />
